44b08bd95587f430b17fc21d58a31ee942455e7e
rafliad <radenistri@gmail.com>
2024-04-01 07:58:27 +0700

    menambahkan class TeamMembers1302223153

---
+++ modul7_kelompok_8/member.cs
@@ -0,0 +1,8 @@
+﻿public class member
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public string nim { get; set; }
+}
